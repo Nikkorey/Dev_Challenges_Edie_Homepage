a9dfb995f815ffea1f3252c6a39dc5af176f9c70
Nikko Rey <nikkoreyhular0818@gmail.com>
2020-08-17 23:18:20 +0400

    updated refactoring

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const nav = document.querySelector(".nav");
 burgerBtn.addEventListener("click", () => {
   nav.classList.toggle("nav__open");
 });
+
+const navLink = document.querySelectorAll(".nav__item-link");
+console.log(navLink);
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav__open");
+  });
+});
